ebf47d109d033d3dd72122599e7235e014f3c364
Taras <kachmar.taras@gmail.com>
2024-06-21 19:01:47 +0300

    add details-restaurant adaptive, fix some images

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,28 +3,6 @@ import Swiper, { Navigation, Autoplay } from 'swiper';
 // import Swiper styles
 import 'swiper/swiper.scss';
 
-// const swiper = new Swiper('.swiper-ltr', {
-//   // modules: [Navigation, Autoplay],
-//   speed: 3500,
-//   slidesPerView: 2,
-//   spaceBetween: 20,
-//   loop: true,
-//   autoplay: {
-//     delay: 2500,
-//     reverseDirection: true,
-//     disableOnInteraction: false,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   // breakpoints: {
-//   //   1024: {
-//   //     spaceBetween: 40,
-//   //   },
-//   // },
-// });
-
 const rtl = new Swiper('.swiper-rtl', {
   modules: [Navigation, Autoplay],
   speed: 2000,
@@ -73,7 +51,8 @@ const ltr = new Swiper('.swiper-ltr', {
 const dr = new Swiper('.details-restaurant__swiper', {
   modules: [Navigation, Autoplay],
   speed: 4000,
-  slidesPerView: 1.3,
+  slidesPerView: 'auto',
+  // initialSlide: 3,
   spaceBetween: 20,
   loop: true,
   autoplay: {
@@ -86,8 +65,10 @@ const dr = new Swiper('.details-restaurant__swiper', {
   },
   breakpoints: {
     1024: {
-      slidesPerView: 2.33,
       spaceBetween: 34,
+    },
+    1280: {
+      spaceBetween: 86,
     },
   },
 });
@@ -95,7 +76,7 @@ const dr = new Swiper('.details-restaurant__swiper', {
 const dh = new Swiper('.details-hotel__swiper', {
   modules: [Navigation, Autoplay],
   speed: 4000,
-  slidesPerView: 2,
+  slidesPerView: 'auto',
   spaceBetween: 20,
   loop: true,
   autoplay: {
