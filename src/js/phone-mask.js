f677b533c9dfc314026b9002af655b9095d68c91
Taras <kachmar.taras@gmail.com>
2024-07-19 16:47:23 +0300

    add favicon

--- src/js/phone-mask.js
+++ src/js/phone-mask.js
@@ -1,8 +1,8 @@
-// import IMask from 'imask';
+import IMask from 'imask';
 
-// const phoneInputEl = document.querySelector('#phone');
-// console.log(phoneInputEl);
+const phoneInputEl = document.querySelector('#phone');
+console.log(phoneInputEl);
 
-// const mask = new IMask(phoneInputEl, {
-//   mask: '+{38}(000)000-00-00',
-// });
+const mask = new IMask(phoneInputEl, {
+  mask: '+{38}(000)000-00-00',
+});
