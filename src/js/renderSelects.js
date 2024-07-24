a4a138acea30e373698e9c252429d882a2b73351
Taras <kachmar.taras@gmail.com>
2024-07-24 03:17:03 +0300

    add booking table render select

--- src/js/renderSelects.js
+++ src/js/renderSelects.js
@@ -1,13 +1,13 @@
 import dayjs from 'dayjs';
 
-const firstDate = document.querySelector('.js-select#first-date');
-const lastDate = document.querySelector('.js-select#last-date');
-
 function bookingRoomRenderSelect() {
+  const arrivalDate = document.querySelector('.js-select#arrival-date');
+  const departureDate = document.querySelector('.js-select#departure-date');
+
   let markup = [];
   let date = dayjs();
 
-  for (let i = 0; i < 365; i++) {
+  for (let i = 0; i < 365; i += 1) {
     let formattedDate = date.format('DD.MM.YYYY');
 
     if (i === 0) {
@@ -19,8 +19,43 @@ function bookingRoomRenderSelect() {
     }
     date = date.add(1, 'day');
   }
-  firstDate.innerHTML = markup.join('');
-  lastDate.innerHTML = markup.join('');
+  arrivalDate.innerHTML = markup.join('');
+  departureDate.innerHTML = markup.join('');
 }
 
 bookingRoomRenderSelect();
+
+function bookingTableRenderSelect() {
+  const reserveTableDatetime = document.querySelector(
+    '.js-select#reserve-table-datetime'
+  );
+
+  console.log(reserveTableDatetime);
+  console.log(dayjs().format('DD.MM.YYYY HH:mm'));
+
+  let datetimeMarkup = [];
+  let date = dayjs();
+
+  for (let i = 0; i < 365; i += 1) {
+    for (let hour = 0; hour < 24; hour += 1) {
+      let formattedDatetime = date
+        .hour(hour)
+        .minute(0)
+        .format('DD.MM.YYYY HH:mm');
+      if (i === 0 && hour === 0) {
+        datetimeMarkup.push(
+          `<option value="${formattedDatetime}" selected>Сьогодні</option>`
+        );
+      } else {
+        datetimeMarkup.push(
+          `<option value="${formattedDatetime}">${formattedDatetime}</option>`
+        );
+      }
+    }
+    date = date.add(1, 'day');
+  }
+
+  reserveTableDatetime.innerHTML = datetimeMarkup.join('');
+}
+
+bookingTableRenderSelect();
